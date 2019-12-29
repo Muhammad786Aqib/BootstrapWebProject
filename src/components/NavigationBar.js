2f634d0d9b40d65d7f4ca609cc78fbb9ecd95b5b
Muhammad786Aqib <aqib.mukhtar70@gmail.com>
2019-12-29 08:05:09 -0800

    bootstrap

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
+import styled from 'styled-components';
+import './CustomNavbar.css'
+
+const Styles = styled.div`
+  .navbar {
+    background-color: #222;
+  }
+
+  a, .navbar-brand, .navbar-nav .nav-link {
+    color: #bbb;
+  &:hover {
+      color: white;
+      
+    }
+  }
+`;
+
+ const NavigationBar = () => (
+  <Styles>
+    <Navbar expand="lg">
+      <Navbar.Brand href="/">Code Life</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/about">About</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/news">News</Link>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles >
+)
+export default NavigationBar;
